4b9d35b070a647e0773a9d6b31890653a8dde6ad
macray <macraykorir254@gmail.com>
2023-07-07 10:06:15 +0300

    Phase1-project

--- js/index.js
+++ js/index.js
@@ -34,51 +34,52 @@ searchForm.addEventListener('submit', function(event) {
         console.error('Error:', error);
       });
   }
-  
 // Function to display the search results on the page
 function displaySearchResults(data) {
-    resultsContainer.innerHTML = '';
-  
-    if (data.docs.length === 0) {
-      resultsContainer.innerHTML = '<p>No results found.</p>';
-      return;
-    }
-  
-    data.docs.forEach(function(book) {
-      const title = book.title || 'Title not available';
-      const author = book.author_name?.[0] || 'Author not available';
-      const coverId = book.cover_i || '';
-      const coverUrl = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
-      const bookId = book.key || '';
-  
-      const bookCard = document.createElement('div');
-      bookCard.classList.add('bookCard');
-      bookCard.innerHTML = `
-        <img src="${coverUrl}" alt="${title} Cover">
-        <h3>${title}</h3>
-        <p>By: ${author}</p>
-        <button class="like-button" data-book-id="${bookId}">
-          <i class="far fa-heart"></i>
-        </button>
-      `;
-  
-      const likeButton = bookCard.querySelector('.like-button');
-      const heartIcon = bookCard.querySelector('.heart-icon');
+  resultsContainer.innerHTML = '';
 
-    // Check if the book is already liked and update the UI accordingly
-    if (likedBooks.includes(bookId)) {
-        likeButton.classList.add('liked');
-        heartIcon.classList.add('liked');
-      }
-  
-      likeButton.addEventListener('click', function() {
-        toggleLike(bookId, likeButton, heartIcon);
-      });
-  
-      resultsContainer.appendChild(bookCard);
-    });
+  if (data.docs.length === 0) {
+    resultsContainer.innerHTML = '<p>No results found.</p>';
+    return;
   }
 
+  const maxResults = 25;
+  const resultsCount = Math.min(data.docs.length, maxResults);
+
+  for (let i = 0; i < resultsCount; i++) {
+    const book = data.docs[i];
+    const title = book.title || 'Title not available';
+    const author = book.author_name?.[0] || 'Author not available';
+    const coverId = book.cover_i || '';
+    const coverUrl = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('bookCard');
+    bookCard.innerHTML = `
+      <img src="${coverUrl}" alt="${title} Cover">
+      <h3>${title}</h3>
+      <p>By: ${author}</p>
+      <button class="like-button" data-book-id="${book.id}">
+        <i class="far fa-heart"></i>
+      </button>
+    `;
+
+    const likeButton = bookCard.querySelector('.like-button');
+    const heartIcon = bookCard.querySelector('.fa-heart');
+    if (likedBooks.includes(book.id)) { // Use book.id instead of bookId
+      likeButton.classList.add('liked');
+      heartIcon.classList.add('liked');
+    }
+
+    likeButton.addEventListener('click', function() {
+      toggleLike(book.id, likeButton, heartIcon); // Use book.id instead of bookId
+    });
+
+    resultsContainer.appendChild(bookCard);
+  }
+}
+
+  
 // Function to toggle the like status of a book
 function toggleLike(bookId, likeButton, heartIcon) {
     if (likedBooks.includes(bookId)) {
